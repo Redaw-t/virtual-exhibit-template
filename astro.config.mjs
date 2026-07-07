9db938b6960aca264299f2ebb32aca57039bf682
Redaw-t <141250635+Redaw-t@users.noreply.github.com>
2026-07-07 23:39:05 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
+  site: 'https://redaw-t.github.io',
   base: 'virtual-exhibit-template',
 });
 
